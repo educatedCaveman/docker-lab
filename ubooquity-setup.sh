8f50737510e729b3029435d489bbdf386417c8f9
drake <j.drake.hoffman@gmail.com>
2020-08-31 14:47:36 -0400

    misc updates

---
+++ ubooquity-setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p /mnt/storage_node/Docker/prd/ubooquity/config
+mkdir -p /mnt/storage_node/Docker/prd/ubooquity/books
+mkdir -p /mnt/storage_node/Docker/prd/ubooquity/comics
+mkdir -p /mnt/storage_node/Docker/prd/ubooquity/files
+
+mkdir -p /mnt/storage_node/Docker/dev/ubooquity/config
+mkdir -p /mnt/storage_node/Docker/dev/ubooquity/books
+mkdir -p /mnt/storage_node/Docker/dev/ubooquity/comics
+mkdir -p /mnt/storage_node/Docker/dev/ubooquity/files
+
