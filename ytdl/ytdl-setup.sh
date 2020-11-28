2a11568f87b7dfb6f4d650e70eb9508b4578628a
Drake <j.drake.hoffman@gmail.com>
2020-11-28 14:59:34 -0500

    organize files

---
+++ ytdl/ytdl-setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir -p /mnt/storage_node/Docker/prd/youtube-dl/appdata
+mkdir -p /mnt/storage_node/Docker/prd/youtube-dl/subscriptions
+mkdir -p /mnt/storage_node/staging/docker/prd/youtube-dl/audio
+mkdir -p /mnt/storage_node/staging/docker/prd/youtube-dl/video
+
+mkdir -p /mnt/storage_node/Docker/dev/youtube-dl/appdata
+mkdir -p /mnt/storage_node/Docker/dev/youtube-dl/subscriptions
+mkdir -p /mnt/storage_node/staging/docker/dev/youtube-dl/audio
+mkdir -p /mnt/storage_node/staging/docker/dev/youtube-dl/video
